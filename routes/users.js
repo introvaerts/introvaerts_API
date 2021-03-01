08e93771d215b82c6cd49cabc1fcc95ab8329915
Jasmin Jenull <jasminjenull@gmail.com>
2021-03-01 16:33:08 +0100

    Add users/create

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const usersController = require("../controllers/users");
+
+const router = express.Router();
+
+router.post("/create", usersController.create);
+
+module.exports = router;
