f6223fd3cba3503476d735b490f5ffceda66b4a6
Jasmin Jenull <jasminjenull@gmail.com>
2021-03-01 16:33:51 +0100

    Update order in index

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
+const usersRoutes = require("./routes/users");
 
-//importing routes
-
-
-// Initializing app and port 
+// Initializing app and port
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Body-Parser config
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//importing routes
+app.use("/users", usersRoutes);
 
 // DotEnv config
 dotenv.config();
 
-// Body-Parser config 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
 // Using th
 
-app.listen(PORT, () => {console.log(`Listening on port ${PORT}`)});
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
