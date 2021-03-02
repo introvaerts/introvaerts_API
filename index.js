3282fc307a2061796a6b979ffd9172d8a6f6d525
Jasmin Jenull <jasminjenull@gmail.com>
2021-03-02 12:43:59 +0100

    Test authentication

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
-const usersRoutes = require("./routes/users");
+const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const usersRoutes = require('./routes/users');
 const connectDB = require('./config/dbConfig');
-const userRoutes = require('./routes/users')
-const subdomainRoutes = require('./routes/subdomains')
+const userRoutes = require('./routes/users');
+const subdomainRoutes = require('./routes/subdomains');
 
 // Initializing app and port
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Body-Parser config
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//importing routes
-app.use("/users", usersRoutes);
 
 // DotEnv config
 dotenv.config();
@@ -23,9 +16,9 @@ dotenv.config();
 // DB config
 connectDB();
 
-// Body-Parser config 
-app.use(bodyParser.urlencoded({ extended: false }));
+// Body-Parser config
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Setup routes (Just to test DB)
 app.use('/users', userRoutes);
