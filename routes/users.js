e297e707de7a359f6cf3553ab9b08e14c155bd86
Montaser Amro <mono.amro@gmail.com>
2021-03-02 13:09:03 +0100

    implements login logic

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const usersController = require('../controllers/users');
 const router = express.Router();
 
 router.post('/create', usersController.create);
+router.post('/login', usersController.login);
 
 module.exports = router;
