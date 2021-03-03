81d97f918082aa0e1f61fe07793ca7793a9311e2
Montaser Amro <mono.amro@gmail.com>
2021-03-03 14:43:05 +0100

    adds error handling and response creation to image upload

--- controllers/images.js
+++ controllers/images.js
@@ -4,23 +4,21 @@ const Image = require('../models/image');
 // Import services
 const imageUploader = require('../services/imageUploader');
 const Form = require('../services/formParser');
+const response = require("../services/response")
 
 const imagesController = {
   upload: async (req, res) => {
     try {
       const buffers = await Form.parseImage(req, res);
       const imageUrls = await imageUploader(buffers);
+
       const imageObjects = imageUrls.map(imageUrl => {
         return { image_url: imageUrl };
       });
       const images = await Image.insertMany(imageObjects);
-      res.json({
-        status: 201,
-        message: `Successfully uploaded ${images.length} images.`,
-        images: images,
-      });
+      res.json(response.uploadImage(images, "Successfully uploaded image."));
     } catch (e) {
-      console.error(e);
+      res.json(response.buildError(e));
     }
   },
 };
