f274df57f1ffed7e0c760bec58be49721c1f09c1
Jasmin Jenull <jasminjenull@gmail.com>
2021-03-04 16:24:09 +0100

    Update delete image

--- controllers/galleries.js
+++ controllers/galleries.js
@@ -1,52 +1,65 @@
-const Gallery = require("../models/gallery");
-const response = require("../services/response");
-const ObjectId = require('mongoose').Types.ObjectId;
+const Gallery = require('../models/gallery');
+const response = require('../services/response');
 
 const galleriesController = {
   create: async (req, res) => {
     const { name } = req.body;
     try {
       const gallery = await Gallery.create({ name });
-      if(gallery) res.json(response.galleryResponse(gallery, 'Gallery created successfully'));
+      if (gallery)
+        res.json(
+          response.galleryResponse(gallery, 'Gallery created successfully')
+        );
     } catch (e) {
-      res.json(response.buildError(e))
+      res.json(response.buildError(e));
     }
   },
   findOne: async (req, res) => {
     try {
       const { galleryId } = req.params;
       gallery = await Gallery.findById(galleryId);
-      res.json(response.galleryResponse(gallery, "Found gallery successfuly"))
+      res.json(response.galleryResponse(gallery, 'Found gallery successfuly'));
     } catch (e) {
-      res.json(response.buildError(e))
+      res.json(response.buildError(e));
     }
   },
   addImage: async (galleryId, imageId) => {
     try {
-      const gallery = await Gallery.findOneAndUpdate({_id: galleryId}, {$push: { images: imageId }}, { new: true });
-      return gallery
+      const gallery = await Gallery.findOneAndUpdate(
+        { _id: galleryId },
+        { $push: { images: imageId } },
+        { new: true }
+      );
+      return gallery;
     } catch (e) {
-      res.json(response.buildError(e))
+      res.json(response.buildError(e));
     }
   },
-  deleteImage: async (req, res) => {
-    const { galleryId, imageId } = req.body;
+  deleteImage: async (galleryId, imageId) => {
     try {
-      const gallery = await Gallery.findOneAndUpdate({_id: galleryId}, {$pull: { images: imageId }}, { new: true });
-      res.json(response.galleryResponse(gallery, "Image removed successfuly"))
+      const gallery = await Gallery.findOneAndUpdate(
+        { _id: galleryId },
+        { $pull: { images: imageId } },
+        { new: true }
+      );
+      return gallery;
     } catch (e) {
-      res.json(response.buildError(e))
+      res.json(response.buildError(e));
     }
   },
   updateName: async (req, res) => {
     const { galleryId, newName } = req.body;
     try {
-      const gallery = await Gallery.findOneAndUpdate({_id: galleryId}, {$set: { name: newName }}, { new: true });
-      res.json(response.galleryResponse(gallery, "Name updated successfuly"))
+      const gallery = await Gallery.findOneAndUpdate(
+        { _id: galleryId },
+        { $set: { name: newName } },
+        { new: true }
+      );
+      res.json(response.galleryResponse(gallery, 'Name updated successfuly'));
     } catch (e) {
-      res.json(response.buildError(e))
+      res.json(response.buildError(e));
     }
-  }
-}
+  },
+};
 
 module.exports = galleriesController;
