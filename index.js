b74d8139fb55e6ae43c7cee980ae934900e774fe
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-03-04 13:39:46 +0100

    move doten config to line 3

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+// DotEnv config
+dotenv.config();
 const bodyParser = require('body-parser');
 const connectDB = require('./config/dbConfig');
 const usersRoutes = require('./routes/users');
@@ -10,13 +12,11 @@ const galleriesRoutes = require('./routes/galleries');
 // Initializing app and port
 const app = express();
 const PORT = process.env.PORT || 3000;
+console.log(process.env.PORT);
 
 // Body-Parser config
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// DotEnv config
-dotenv.config();
 
 // DB config
 connectDB();
