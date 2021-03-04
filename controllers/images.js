cc8cc543975152dd358eedd7f5639242c349b667
Jasmin Jenull <jasminjenull@gmail.com>
2021-03-04 08:41:02 +0100

    Remove console.log

--- controllers/images.js
+++ controllers/images.js
@@ -5,10 +5,10 @@ const Gallery = require('../models/gallery');
 // Import services
 const imageUploader = require('../services/imageUploader');
 const form = require('../services/formParser');
-const response = require("../services/response")
+const response = require('../services/response');
 
 // Import actions from galleriesController
-const { addImage } = require('./galleries')
+const { addImage } = require('./galleries');
 
 const imagesController = {
   upload: async (req, res) => {
@@ -16,7 +16,6 @@ const imagesController = {
       const parsedImage = await form.parseImage(req);
       const imageObject = await imageUploader(parsedImage);
       const image = await Image.create(imageObject);
-      console.log(image)
       const gallery = await addImage(imageObject.gallery_id, image._id);
       res.json({
         status: 201,
@@ -26,7 +25,7 @@ const imagesController = {
     } catch (e) {
       res.json(response.buildError(e));
     }
-  }
+  },
 };
 
 module.exports = imagesController;
