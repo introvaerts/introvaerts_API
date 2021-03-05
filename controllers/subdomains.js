35deac52237a24f6a644e124f508c848e97bf010
Jasmin Jenull <jasminjenull@gmail.com>
2021-03-05 11:19:41 +0100

    Add create subdomain function

--- controllers/subdomains.js
+++ controllers/subdomains.js
@@ -1,7 +1,29 @@
+// Import models
 const Subdomain = require('../models/subdomain');
 const User = require('../models/user');
 
+// Import services
+const response = require('../services/response');
+
 const subdomainsController = {
+  create: async (req, res) => {
+    try {
+      subdomain = await Subdomain.create(req.body);
+      user = await User.findOneAndUpdate(
+        { _id: req.user_id },
+        { $push: { subdomains: subdomain._id } },
+        { new: true }
+      );
+      res.json(
+        response.subdomainResponse(
+          subdomain,
+          `Successfully created subdomain ${subdomain.name}`
+        )
+      );
+    } catch (e) {
+      res.json(response.buildError(e));
+    }
+  },
   getAllByUser: async (req, res) => {
     const user = await User.findById(req.user_id);
     const subdomains = await Subdomain.find({
