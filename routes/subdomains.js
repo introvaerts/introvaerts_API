122c6bff74e82fbb3fa84b6507b4e59b7ceb50de
Jasmin Jenull <jasminjenull@gmail.com>
2021-03-05 14:30:44 +0100

    Add patch route for update

--- routes/subdomains.js
+++ routes/subdomains.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.use(authenticateUser);
 router.post('/create', subdomainsController.create);
+router.patch('/update/:id', subdomainsController.update);
 router.get('/', subdomainsController.getAllByUser);
 
 module.exports = router;
