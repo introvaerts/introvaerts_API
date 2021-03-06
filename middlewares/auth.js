c902e862546d71f0a30bc625061c9b6815a7f419
Montaser Amro <mono.amro@gmail.com>
2021-03-06 14:01:38 +0100

    Added authentication to Gallery routes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,15 @@
 const jwt = require('jsonwebtoken');
+const response = require('../services/response');
 
 const authenticateUser = (req, res, next) => {
-  const token = req.header('Authorization');
-  const decodedToken = jwt.verify(token, process.env.JWT_SIGNATURE);
-  req.user_id = decodedToken.user_id;
-  next();
+  try {
+    const token = req.header('Authorization');
+    const decodedToken = jwt.verify(token, process.env.JWT_SIGNATURE);
+    req.user_id = decodedToken.user_id;
+    next();
+  } catch (e) {
+    res.send(response.buildError(e))
+  }
 };
 
 module.exports = authenticateUser;
