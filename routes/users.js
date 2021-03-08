172eac78080484ee96d8d8d54e74a825b15ff273
Jasmin Jenull <jasminjenull@gmail.com>
2021-03-08 11:52:21 +0100

    Add get one user and update email route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const usersController = require('../controllers/users');
+const authenticateUser = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.post('/create', usersController.create);
 router.post('/login', usersController.login);
+router.get('/account', authenticateUser, usersController.getInfo);
+router.patch('/account', authenticateUser, usersController.updateEmail);
 
 module.exports = router;
