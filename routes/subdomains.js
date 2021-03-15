fb92f92001e04b5d4c99fc55c3bb1161af97da52
Jasmin Jenull <jasminjenull@gmail.com>
2021-03-15 15:35:16 +0100

    Add route to publish

--- routes/subdomains.js
+++ routes/subdomains.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/:id', subdomainsController.findOne);
 router.get('/names/:name', subdomainsController.findByName);
+router.get('/publish/:id', authenticateUser, subdomainsController.publish);
 router.get('/', authenticateUser, subdomainsController.getAllByUser);
 router.post('/create', authenticateUser, subdomainsController.create);
 router.patch('/:id', authenticateUser, subdomainsController.update);
